feed69d10c41cb84ca5869c469e6ff72be82dcaa
PikaYuhno <muazahmed019@gmail.com>
2021-06-21 23:16:00 +0200

    Fixed docker issues

--- packages/server/src/constants.ts
+++ packages/server/src/constants.ts
@@ -3,4 +3,4 @@ export const SESSION_USERID_PREFIX = "userIds:";
 export const SESSION_PREFIX = "sess:"
 export const CONFIRMATION_CODE_PREFIX = "confirmCode:";
 // @todo add production ip 
-export const CLIENT_HOST = process.env.NODE_ENV === "production" ? "" : "http://localhost:3000";
+export const CLIENT_HOST = process.env.NODE_ENV === "production" ? "http://localhost" : "http://localhost:3000";
--- packages/web/src/constants.ts
+++ packages/web/src/constants.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = "";
+export const BASE_URL = "http://localhost:4000";
